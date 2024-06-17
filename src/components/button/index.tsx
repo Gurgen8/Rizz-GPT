110c3dc11b943364005cb02d6bd513cf081ae629
Gurgen8 <mkrtchyangurgen8@mail.ru>
2024-06-18 01:17:58 +0400

    finish ui all

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,25 @@
 import React, {FC, memo} from 'react';
-import {ButtonView} from './styles';
+import {ButtonView, ShadowStyles} from './styles';
 import {LocalProps} from './types';
 import {AppText} from '@uiKit';
+import {View} from 'react-native';
 
 export const Button: FC<LocalProps> = memo(({text, isLeftText, onPress}) => {
-  return (
-    <ButtonView onPress={onPress} isLeftText={isLeftText}>
+  const renderLightShadow = () => (
+    <View style={ShadowStyles}>
+      <ButtonView
+        style={ShadowStyles}
+        onPress={onPress}
+        isLeftText={isLeftText}>
+        <AppText variant="h5" text={text} />
+      </ButtonView>
+    </View>
+  );
+
+  return isLeftText ? (
+    renderLightShadow()
+  ) : (
+    <ButtonView style={ShadowStyles} onPress={onPress}>
       <AppText variant="h5" text={text} />
     </ButtonView>
   );
--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -1,11 +1,26 @@
 import styled from 'styled-components/native';
 import {Colors} from '@uiKit';
+import DropShadow from 'react-native-drop-shadow';
 
-export const ButtonView = styled.TouchableOpacity<{isLeftText?: boolean}>`
+const ShadowStyles = {
+  shadowColor: Colors.WHITE,
+  shadowOffset: {
+    width: -0.5,
+    height: -1,
+  },
+  shadowOpacity: 1,
+  shadowRadius: 3,
+  width: 280,
+};
+
+const StartedButton = styled(DropShadow)``;
+
+const ButtonView = styled.TouchableOpacity<{isLeftText?: boolean}>`
   background-color: ${Colors.GREEN};
   padding: 15px 30px;
-  border-radius: 30px;
+  border-radius: 17px;
   align-items: ${props => (props.isLeftText ? 'none' : 'center')};
   margin-top: 20px;
   width: 100%;
 `;
+export {ButtonView, StartedButton, ShadowStyles};
